512ad72b335169308fe6424b1de7324d71b17df5
T.Kigi <tatsuya.kigi@gmail.com>
2020-06-07 00:51:25 +0900

    change words for search

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "シャニマスフェスシミュレーター(β)",
+    title: "シャニマス フェス計算機(β)",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
