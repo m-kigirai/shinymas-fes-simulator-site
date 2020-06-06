ae2bc3fb6e5d3a7c14aac79351efaa3b3297f204
T.Kigi <tatsuya.kigi@gmail.com>
2020-06-07 00:36:37 +0900

    add robots.txt and modify html head content

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,15 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: "シャニマスフェスシミュレーター(β)",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content:
+          "THE iDOLM@STER ShinyColorsのフェスモードのアピール計算を行えるサイトです"
       }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
